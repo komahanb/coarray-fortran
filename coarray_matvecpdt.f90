5a598cfb42d205d4f8e03211e64fb7fc1d31c455
Komahan Boopathy <komahan.cool@gmail.com>
2018-04-19 13:05:41 -0400

    corrected matvec product

--- coarray_matvecpdt.f90
+++ coarray_matvecpdt.f90
@@ -8,12 +8,13 @@ program test_norm
 
   implicit none
   
-  integer, parameter :: global_size = 4
+  integer, parameter :: global_size = 4000000
 
   ! Distributed work data
   real(8), allocatable :: A(:,:)
   real(8), allocatable :: x(:)
   real(8), allocatable :: b(:)
+  real(8), allocatable :: btmp(:)
 
   integer :: nimages = 1
   integer :: local_size
@@ -27,39 +28,23 @@ program test_norm
   ! Decompose matrix columnwise and vector rowwise
   allocate(A(global_size,local_size))
   allocate(x(local_size))
-
-!!$  print *, shape(A), this_image()
-!!$  print *, shape(x), this_image()
+  allocate(b(local_size))
+  allocate(btmp(global_size))
   
   ! Fill the local arrays with random values or known entries
   call random_number(A)
   call random_number(x)
 
-!!$  if (this_image() == 1) then
-!!$     A(:,1) = [1.0d0, 2.0d0, 3.0d0, 4.0d0]
-!!$     A(:,2) = 2.0d0*[1.0d0, 2.0d0, 3.0d0, 4.0d0]
-!!$     x(1:2) = [1.0d0, 2.0d0]
-!!$  else
-!!$     A(:,1) = 3.0d0*[1.0d0, 2.0d0, 3.0d0, 4.0d0]
-!!$     A(:,2) = 4.0d0*[1.0d0, 2.0d0, 3.0d0, 4.0d0]
-!!$     x(1:2) = [3.0d0, 4.0d0]
-!!$  end if
-
   ! Matrix vector multiplication
-  b = matmul(A, x)
-  print *, "A=", A, "from", me
-  print *, "x=", x, "from", me
-  print *, "b=", b, "from", me
+  btmp = matmul(A, x)
   
   ! Compiler does not support array reduction yet!
-   do i = 1, global_size
-     call co_reduce (b(i), operator=sum)
-  end do
+  call co_reduce (btmp, operator=sum)
+  
+  b = btmp((me-1)*local_size+1:me*local_size)
+  !write(*,*) "the new vector is", b , this_image()
 
-  ! print b = Ax vector
-  if (this_image() == 1) then
-     write(*,*) "the new vector is", b , this_image()
-  end if
+  deallocate(A,x,b,btmp)
   
 contains
 
